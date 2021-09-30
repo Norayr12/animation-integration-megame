98eb2758ea700a2028064482eaea5c7bec316b41
Norayr <norajrhacatran8@gmail.com>
2021-09-30 15:24:46 +0400

    Movement Animations, Bug Fix

--- Assets/AnimationIntegration/Scripts/Player.cs
+++ Assets/AnimationIntegration/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
 
     [Header("Player settings")]
     [SerializeField] private float _playerSpeed;
+    [SerializeField] private float _rotationSpeed;
 
     private int _horizontalDirection, _verticalDirection; 
 
@@ -23,9 +24,16 @@ public class Player : MonoBehaviour
     {
         float rightLeft = Input.GetAxis("Horizontal");
         float backForward = Input.GetAxis("Vertical");
+        
         _animator.SetFloat(_horizontalDirection, rightLeft);
-        _animator.SetFloat(_verticalDirection, backForward);
+        if(backForward == 0 && rightLeft != 0)
+            _animator.SetFloat(_verticalDirection, rightLeft != 0 ? backForward + 0.1f : backForward);
+        else
+            _animator.SetFloat(_verticalDirection, backForward);
 
         transform.Translate(new Vector3(rightLeft, 0, backForward) * _playerSpeed * Time.fixedDeltaTime);
+
+        Vector3 rotation = backForward > 0 ? new Vector3(0, _rotationSpeed * rightLeft * Time.fixedDeltaTime, 0) : new Vector3(0, -_rotationSpeed * rightLeft * Time.fixedDeltaTime, 0);
+        transform.Rotate(backForward != 0 ? rotation : Vector3.zero);
     }
 }
